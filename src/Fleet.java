465509fab54256b1b5c7ad4bfe2ae9c3388ce4ef
William Clifford <w.big.red.dog@gmail.com>
2023-02-13 23:47:07 -0600

    Added implementation of loadStarships method, added parseData method

--- src/Fleet.java
+++ src/Fleet.java
@@ -1,9 +1,11 @@
+import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Fleet {
 	private String name;
-	ArrayList<Starship> fleet;
+	public ArrayList<Starship> fleet;
 	
 	public Fleet(String nm){
 		this.name = nm;
@@ -29,6 +31,52 @@ public class Fleet {
 		 * add a starship to the current fleet for every file found
 		 * 		this will use the information in the found file to get the information for the starship object
 		 * */
+		File dataDir = new File(dirName);
+		String[] filenames = dataDir.list();
+		String[] tokens;
+		//Scanner read;
+		String lineIn;
+		
+		for(int i = 0; i < filenames.length; ++i) {
+			System.out.println(filenames[i]);
+			try {
+				Scanner read = new Scanner(new File(dirName + "/" + filenames[i]));
+				while(read.hasNextLine()) {
+					lineIn = read.nextLine();
+					//System.out.println(lineIn);
+					tokens = lineIn.split(",");
+					parseData(tokens, i);
+					for(int j = 0; j < tokens.length; ++j) {
+						System.out.println(tokens[j]);
+					}
+					System.out.println("");
+				}
+				read.close();
+			}
+			catch(FileNotFoundException e){
+				e.printStackTrace();
+			}
+		}
+	}
+	
+	private void parseData(String[] tokens, int state) {
+		switch(state) {
+			case 0:
+				Starship tempShip = new Starship(tokens[0], tokens[1], tokens[2]);
+				addStarship(tempShip);
+				break;
+			default:
+				if(tokens.length == 4) {
+					CrewMember newCrew = new CrewMember(tokens[0], tokens[1], tokens[2], tokens[3]);
+					fleet.get(fleet.size() - 1).addCrewMember(newCrew);
+					System.out.println();
+				}
+				else if(tokens.length == 5){
+					CrewMember newCrew = new CrewMember(tokens[0], tokens[1], tokens[2], tokens[3], tokens[4]);
+					fleet.get(fleet.size()).addCrewMember(newCrew);
+				}	
+				break;
+		}
 	}
 	
 	public String getName() {
